50e8ccf711c5e1313ea721bb55a5524375827572
alfe <alfe10251@gmail.com>
2022-07-30 16:17:29 +0900

    add: footer

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,29 @@ function App() {
   const [text, setText] = useState('文字列');
 
   return (
-    <div className="App">
-      <span>{text}</span>
-      <EditButtonWithConfirm
-        tooltip="編集"
-        onSubmit={(value, successCallback) => {
-          setText(value.text);
-          successCallback();
-        }}
-      />
-      <DeleteButtonWithConfirm
-        tooltip="削除"
-        confirmTitle="削除確認ダイアログ"
-        confirmText="対象の文字列を削除しますか？"
-        onSubmit={() => setText('')}
-      />
-    </div>
+    <>
+      <div className="App">
+        <span>{text}</span>
+        <EditButtonWithConfirm
+          tooltip="編集"
+          onSubmit={(value, successCallback) => {
+            setText(value.text);
+            successCallback();
+          }}
+        />
+        <DeleteButtonWithConfirm
+          tooltip="削除"
+          confirmTitle="削除確認ダイアログ"
+          confirmText="対象の文字列を削除しますか？"
+          onSubmit={() => setText('')}
+        />
+      </div>
+      <footer className="App-footer">
+        <a href="https://github.com/alfe/react-confirmation-dialog-sample">
+          https://github.com/alfe/react-confirmation-dialog-sample
+        </a>
+      </footer>
+    </>
   )
 }
 
